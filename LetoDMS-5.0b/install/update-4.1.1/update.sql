bb6086b897b42dfef10e800133438bba74b53cc8
matinmajtavi <matin.majtavi@trilex.ca>
2026-08-25 16:47:53 -0400

    cleaning master
    cleaning master

--- LetoDMS-5.0b/install/update-4.1.1/update.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `tblPermissions` (`name`, `description`) VALUES
-('navigation.content', 'Show the Content navigation item'),
-('navigation.my_documents', 'Show the My Documents navigation item'),
-('navigation.calendar', 'Show the Calendar navigation item'),
-('navigation.help', 'Show the Help navigation item');
